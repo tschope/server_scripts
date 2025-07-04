5c0838af2821a50a91e0d4da2a8d1d7098d09a58
Rodrigo Tschope <r_tschope@apple.com>
2025-07-04 11:32:15 +0100

    script to deploy application

---
+++ deploy_at_the_server.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -e
+
+echo "🚀 Deployment started ..."
+
+BASE_DIR="/var/www/domain.com"
+RELEASES_DIR="$BASE_DIR/releases"
+TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+NEW_RELEASE="$RELEASES_DIR/$TIMESTAMP"
+CURRENT_LINK="$BASE_DIR/current"
+REPO_URL="git@github.com:tschope/domain.com.git"
+
+# Create new release folder
+mkdir -p "$RELEASES_DIR"
+
+echo "📦 Cloning repo into new release: $NEW_RELEASE"
+git clone --branch main --depth 1 "$REPO_URL" "$NEW_RELEASE"
+
+# Link .env from base folder
+echo "🔗 Linking .env"
+ln -sfn "$BASE_DIR/.env" "$NEW_RELEASE/.env"
+ln -sfn "$BASE_DIR/.env" "$NEW_RELEASE/frontend/.env"
+
+# Set the correct PHP version
+echo "🧰 Setting PHP version"
+sudo update-alternatives --set php /usr/bin/php8.4
+
+# Install Composer Dependencies
+echo "📦 Installing composer dependencies"
+cd "$NEW_RELEASE"
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Run migrations
+echo "🧬 Running migrations"
+php artisan migrate --force
+
+# Build frontend
+echo "🌐 Building frontend"
+cd frontend
+npm install
+npm run build
+cd ..
+
+# Fix folder permissions
+echo "🛠️   Fixing permissions"
+sudo chown -R www-data:www-data .
+sudo find storage -type d -exec chmod 775 {} \; || true
+sudo find storage -type f -exec chmod 664 {} \; || true
+
+# Update current symlink
+echo "🔗 Updating current symlink"
+ln -sfn "$NEW_RELEASE" "$CURRENT_LINK"
+
+# Restart PM2 www application
+cd "$CURRENT_LINK/frontend"
+echo "🚀 Restarting PM2"
+sudo -u www-data pm2 del aviationhub_www
+sudo -u www-data pm2 startOrRestart "$BASE_DIR/ecosystem.config.cjs" --only aviationhub_www
+
+# Restart Nginx to get the new release
+echo "🚀 Restarting Nginx"
+sudo systemctl restart nginx
+
+echo "🧹 Cleaning up old releases (keeping last 3)"
+cd "$RELEASES_DIR"
+sudo ls -1dt */ | tail -n +4 | xargs -I {} sudo rm -rf "{}"
+
+echo "🎉 Deployment complete. Active release: $TIMESTAMP"
